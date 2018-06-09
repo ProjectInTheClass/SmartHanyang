058238090568164e8f7e75c38faea27406e207d1
gameegg <whddnjs2860@naver.com>
2018-06-09 15:41:48 +0900

    .

--- Pods/Target Support Files/Pods-SmartHanYang/Pods-SmartHanYang-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SmartHanYang : NSObject
-@end
-@implementation PodsDummy_Pods_SmartHanYang
-@end
--- README.md
+++
@@ -1,9 +0,0 @@
-# SmartHanyang
-
-
-기존 한양대 앱이 너무 구려서 만드는 더 똑똑한 한양대 앱.
-
-
-오픈소스
---------
-WKZombie - (https://github.com/mkoehnke/WKZombie)
